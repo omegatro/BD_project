4b9c76efb6844a5a8c835657d3f7c6ecbe13e350
omegatro <omegatro@gmail.com>
2023-11-01 10:40:21 +0000

    Added scripts to add label and metadata collections

---
+++ scripts/add_label_table.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+MONGO_USER = os.environ.get("MONGO_USER")
+MONGO_PASS = os.environ.get("MONGO_PASS")
+LABEL_PATH = os.environ.get("LABEL_PATH")
+
+
+# Connect to MongoDB
+client = MongoClient(f'mongodb://{MONGO_USER}:{MONGO_PASS}@localhost:27017/')
+
+#create database
+db = client['big_data_project']
+
+#add collection to store image metadata
+patients_collection = db['labels']
+
+#import dataset from csv file using pandas
+patients_df = pd.read_csv(LABEL_PATH)
+
+# Convert DataFrame to a list of dictionaries
+patients_data = patients_df.to_dict(orient='records')
+
+#insert multiple rows into MongoDB
+patients_collection.insert_many(patients_data)
